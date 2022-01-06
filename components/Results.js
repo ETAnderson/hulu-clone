9d6ca7ec29cb521b80a4bdaac9a5187b5e0f50ba
ETAnderson <anderson.eric.t@gmail.com>
2022-01-06 15:20:20 -0500

    build complete
    finished results and build. pre-deployment

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,15 @@
 import Thumbnail from "./Thumbnail"
+import FlipMove from "react-flip-move"
 
 function Results({ results }) {
     return (
-        <div>
+        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
             {/* Thumbnail */}
             {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
             ))}
             
-        </div>
+        </FlipMove>
     )
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,12 +1,16 @@
 import Image from "next/image"
 import { ThumbUpIcon } from '@heroicons/react/outline'
+import { forwardRef } from "react";
 
-function Thumbnail({ result }) {
+
+const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 
     return (
-        <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div 
+            ref={ref}
+            className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image 
                 src={
                     `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -29,7 +33,7 @@ function Thumbnail({ result }) {
                 </p>
             </div> 
         </div>
-    )
-}
+    );
+})
 
-export default Thumbnail
+export default Thumbnail;
